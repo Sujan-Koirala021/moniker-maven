d1bb3aa644f19c2179f29ef6bd8eb1930dc94390
Sujan <077bct087.sujan@pcampus.edu.np>
2024-05-16 22:32:25 +0545

    Add navbar temp

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
-import './App.css';
-import NavBar from './components/NavBar';
-
+import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <div className='text-red-600 text-xl'>
-
-      Hello world!
-      </div>
+    <div className="app">
+      <NavBar />
     </div>
   );
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,26 +1,81 @@
+import React, { useState } from "react";
+import {
+  PaperAirplaneIcon,
+  MoonIcon,
+  SunIcon,
+  Bars3Icon,
+} from "@heroicons/react/24/outline";
 
+function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-import React from 'react'
-
-export default function NavBar() {
   return (
-    <div className="nav-bar">
-      <div className="container">
-        {/* <img src={Logo} alt="" className="logo" /> */}
-        <div className="categories">
-          <ul>
-            <li>Culture</li>
-            <li>Science</li>
-            <li>Sports</li>
-
-          </ul>
-
-
+    <nav>
+      <div className="max-w-7xl mx-auto">
+        <div className="flex mx-auto justify-between w-5/6">
+          {/* Primary menu and logo */}
+          <div className="flex items-center gap-16 my-12">
+            {/* logo */}
+            <div>
+              <a
+                href="/"
+                className="flex gap-1 font-bold text-gray-700 items-center"
+              >
+                <PaperAirplaneIcon className="h-6 w-6 text-primary" />
+                <span>Paper.io</span>
+              </a>
+            </div>
+            {/* primary */}
+            <div className="hidden lg:flex gap-8">
+              <a href="#" className="">
+                Home
+              </a>
+              <a href="#">Benefits</a>
+              <a href="#">Our Classes</a>
+              <a href="#">Contact Us</a>
+            </div>
+          </div>
+          {/* secondary */}
+          <div className="flex gap-6">
+            <div className="hidden xs:flex items-center gap-10">
+              <div className="hidden lg:flex items-center gap-2">
+                <MoonIcon className="h-6 w-6" />
+                <SunIcon className="h-6 w-6" />
+              </div>
+              <div>
+                <button className="rounded-full border-solid border-2 border-gray-300 py-2 px-4 hover:bg-gray-700 hover:text-gray-100">
+                  Free Trial
+                </button>
+              </div>
+            </div>
+            {/* Mobile navigation toggle */}
+            <div className="lg:hidden flex items-center">
+              <button onClick={() => setToggleMenu(!toggleMenu)}>
+                <Bars3Icon className="h-6" />
+              </button>
+            </div>
+          </div>
         </div>
-        <span>User1</span>
-          <span>Logout</span>
-          <span >Create</span>
       </div>
-    </div>
-  )
+      {/* mobile navigation */}
+      <div
+        className={`fixed z-40 w-full  bg-gray-100 overflow-hidden flex flex-col lg:hidden gap-12  origin-top duration-700 ${
+          !toggleMenu ? "h-0" : "h-full"
+        }`}
+      >
+        <div className="px-8">
+          <div className="flex flex-col gap-8 font-bold tracking-wider">
+            <a href="#" className="border-l-4 border-gray-600">
+              Features
+            </a>
+            <a href="#">Pricing</a>
+            <a href="#">Download</a>
+            <a href="#">Classic</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
+
+export default Navbar;

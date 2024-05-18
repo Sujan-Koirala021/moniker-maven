025e52eae1b9d069855da567ec622e6d42b4a690
Sujan <077bct087.sujan@pcampus.edu.np>
2024-05-18 22:06:46 +0545

    Add aboutus section

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -4,12 +4,15 @@ import Footer from '../components/Footer'
 import NameGenerator from './DirectInput'
 import Hero from '../components/Hero'
 import ContactUs from './ContactUs'
+import AboutUs from './AboutUs'
+
 const LandingPage = () => {
     return (
         <>
                     <Navbar />
                     {/* <Hero/> */}
-                    <ContactUs/>
+                    {/* <ContactUs/> */}
+                    <AboutUs/>
             <Footer/>
         </>
     )
